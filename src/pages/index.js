e08bcdc39f37e516d2ee8c85038e56abaaf308e3
nithinacharya11 <nithinpacharya@gmail.com>
2023-12-06 11:26:15 +0530

    srf

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,16 @@ import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'product one',
+  description: 'test description for product one',
+  openGraph: {
+    images: ['/https://ik.imagekit.io/egrcxziyv/avatar/mercedez_home.jpg'],
+    title: "opengrapgh title for product 1",
+    description:"opengrapoh descrioption for prodcut 1"
+  },
+}
+
 export default function Home() {
   const router = useRouter();
   return (
