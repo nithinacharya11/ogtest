38f2a430a845fb2afa595a5a7ed4519f340e5996
nithinacharya11 <nithinpacharya@gmail.com>
2023-12-06 11:18:32 +0530

    bug fied

--- src/pages/product/[name].js
+++ src/pages/product/[name].js
@@ -5,7 +5,7 @@ export const metadata = {
   title: 'product one',
   description: 'test description for product one',
   openGraph: {
-    images: ['/https://ik.imagekit.io/egrcxziyv/avatar/mercedez_home.jpg', ...previousImages],
+    images: ['/https://ik.imagekit.io/egrcxziyv/avatar/mercedez_home.jpg'],
     title: "opengrapgh title for product 1",
     description:"opengrapoh descrioption for prodcut 1"
   },
@@ -14,8 +14,6 @@ export const metadata = {
 
 const Product = () => {
   const router = useRouter()
-
-
 
      return (
     <div>
