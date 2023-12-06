c930c52328ab9c19ff862f4916f704411e9005b5
nithinacharya11 <nithinpacharya@gmail.com>
2023-12-06 11:17:54 +0530

    teest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  domains: ['ik.imagekit.io'],
 }
 
 module.exports = nextConfig
--- src/pages/product/[name].js
+++ src/pages/product/[name].js
@@ -1,9 +1,23 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
+export const metadata = {
+  title: 'product one',
+  description: 'test description for product one',
+  openGraph: {
+    images: ['/https://ik.imagekit.io/egrcxziyv/avatar/mercedez_home.jpg', ...previousImages],
+    title: "opengrapgh title for product 1",
+    description:"opengrapoh descrioption for prodcut 1"
+  },
+}
+
+
 const Product = () => {
   const router = useRouter()
-  return (
+
+
+
+     return (
     <div>
       <p>product page for {router.query.name}</p>
     </div>
